cb00b2eef4cddb0b1de8c20baf67c33a69810837
chowdhurytanisha98 <auw242061@auw.edu.bd>
2026-02-06 19:54:56 +0845

    Add files via upload

---
+++ eve.c
@@ -0,0 +1,18 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main()
+{
+    int n = get_int("Enter a number: ");
+
+    if (n % 2 == 0)
+    {
+        printf("Even\n");
+    }
+    else
+    {
+        printf("Odd\n");
+    }
+
+    return 0;
+}
