47243ba2de4aa957af16496f6de2ccb3011e986a
chowdhurytanisha98 <auw242061@auw.edu.bd>
2026-02-06 19:32:47 +0845

    Add files via upload

---
+++ patt3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main()
+{
+    int num = 1;
+
+    for (int i = 1; i <= 4; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            printf("%d ", num);
+            num++;
+        }
+        printf("\n");
+    }
+    return 0;
+}
