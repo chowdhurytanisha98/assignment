6c1f019daf6ed77cb2a747c3ba4f25f4abad859f
chowdhurytanisha98 <auw242061@auw.edu.bd>
2026-02-06 19:48:49 +0845

    Add files via upload

---
+++ leap.c
@@ -0,0 +1,18 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main()
+{
+    int year = get_int("Enter a year: ");
+
+    if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+    {
+        printf("Leap year\n");
+    }
+    else
+    {
+        printf("Not a leap year\n");
+    }
+
+    return 0;
+}
