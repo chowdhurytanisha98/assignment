aa0d481e62cca4d34ea8b0a976d65e04ca47c85a
chowdhurytanisha98 <auw242061@auw.edu.bd>
2026-02-06 19:34:09 +0845

    Add files via upload

---
+++ odd2.c
@@ -0,0 +1,19 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main()
+{
+    int n = get_int("Enter n: ");
+    int sum = 0;
+
+    for (int i = 1; i <= n; i++)
+    {
+       if(i%2!=0)
+       {
+        sum=sum+i;
+       }
+    }
+
+    printf("Sum = %d\n", sum);
+    return 0;
+}
